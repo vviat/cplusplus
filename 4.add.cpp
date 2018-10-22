4d8d6370875f7e9e912094b35db639b1577737ca
ygy <40139861+vviat@users.noreply.github.com>
2018-10-22 17:24:20 +0800

    两数相加。ｃｐｐ

---
+++ 4.add.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: 4.add.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年10月17日 星期三 09时56分30秒
+ ************************************************************************/
+
+#include<iostream>
+#include <vector>
+#include<map>
+using std::vector;
+using std::map;
+using std::endl
+using std::cout;
+
+int main(){
+    vector<int> arr;
+    map<int ,int>;
+    int n = 100;
+    while(n--) {
+        arr.push_back(n);
+        mp[n + 100] = n;
+    }
+    for(auto x : arr) {
+        cout << x << endl;
+    }
+    for(auto x : mp) {
+        cout << x << endl;
+    }
+    return 0;
+}
+
